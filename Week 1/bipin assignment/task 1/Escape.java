0e8e414151a67becdabf810efa70a12c480a6f2c
bipin-mysterio <forcollegepurpose1999@gmail.com>
2025-09-07 15:00:07 +0545

    commiting

---
+++ Week 1/bipin assignment/task 1/Escape.java
@@ -0,0 +1,12 @@
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("A \"quoted\" String is");
+        System.out.println("'much' better if you learn");
+        System.out.println("the rules of \"escape sequences.\"");
+        System.out.println("Also, \"\" represents an empty");
+        System.out.println("String. Don't forget: use \\\" ");
+        System.out.println("instead of \" !");
+        System.out.println("'' is not the same as \"");
+    }
+}
+
---
+++ Week 1/bipin assignment/task 1/PatternOne.java
@@ -0,0 +1,11 @@
+public class PatternOne {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
+
+
