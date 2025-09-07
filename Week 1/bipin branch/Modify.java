c734803aa41e3bae8bcd97a15f7f0f7cbd93ec8b
bipin-mysterio <forcollegepurpose1999@gmail.com>
2025-09-07 21:36:56 +0545

    commiting

---
+++ Week 1/bipin branch/Modify.java
@@ -0,0 +1,8 @@
+public class Modify {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.print("This is my first program");
+        System.out.print("I am on module ST4003CEM");
+    }
+}
+
---
+++ Week 1/bipin branch/Paragraph.java
@@ -0,0 +1,13 @@
+public class Paragraph {
+    public static void main(String[] args) {
+        System.out.println("A well-formed Java program has a main method with { and } braces.");
+        System.out.println("A System.out.println statement has ( and ) and usually a String that starts and");
+        System.out.println("ends with a \" character.");
+        System.out.println("(But we type \\\" instead!)");
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"?  Or between a \" and a \\\"?");
+        System.out.println("One is what we see when we're typing our program. The other is what appears");
+        System.out.println("on the \"console.\"");
+    }
+}
+
