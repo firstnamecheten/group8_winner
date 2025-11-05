a3421898f32949cf22a561fc34861a9ac8c4fa25
firstnamecheten <firstnamecheten@yahoo.com>
2025-11-05 13:56:13 +0545

    updated

---
+++ Week 1/cheten_branch/2nd task Loops in Java/ATMVerification.java
@@ -0,0 +1,30 @@
+// Question 7. Write a Java program that simulates an ATM PIN verification system.
+// The program should have a correct PIN stored (for example, 1234) and allow the user up to three attempts to enter the correct PIN.
+// If the user enters the correct PIN, print "Access Granted" and stop the program.
+// If the user enters the wrong PIN, print "Incorrect PIN, try again".
+// After three incorrect attempts, print "Account locked".
+
+import java.util.Scanner;
+
+public class ATMVerification {
+    public static void main(String[] args) {
+        final int CORRECT_PIN = 1234;
+        Scanner sc = new Scanner(System.in);
+        int attempts = 0;
+
+        while (attempts < 3) {
+            System.out.print("Enter PIN: ");
+            int enteredPin = sc.nextInt();
+
+            if (enteredPin == CORRECT_PIN) {
+                System.out.println("Access Granted");
+                return;
+            } else {
+                System.out.println("Incorrect PIN, try again");
+                attempts++;
+            }
+        }
+
+        System.out.println("Account locked");
+    }
+}
---
+++ Week 1/cheten_branch/2nd task Loops in Java/SkipMultiplesOf4.java
@@ -0,0 +1,15 @@
+// Write a Java program that takes a number from the user and prints all numbers from 1 to that number, but skips printing multiples of 4 using an if condition inside a for loop.
+
+import java.util.Scanner;
+public class SkipMultiplesOf4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            if (i % 4 == 0) continue;
+            System.out.println(i);
+        }
+    }
+}
