6e06651b55f9f9c37b933bfefa2d644bfa003e1f
firstnamecheten <firstnamecheten@yahoo.com>
2025-11-06 21:05:11 +0545

    Merged branch_Cheten into main

---
+++ Week 1/cheten_branch/Question5.java
@@ -0,0 +1,9 @@
+public class Question5 {
+    public static void main(String[] args) {
+        System.out.println("********* \n");
+        System.out.println("********* \n");
+        System.out.println("********* \n");
+        System.out.println("********* \n");
+        System.out.println("*********");
+}
+}
